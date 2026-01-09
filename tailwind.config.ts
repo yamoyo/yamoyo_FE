21bdae1efb8bfd2b0cc0797cfc3b8cea3bebd738
junye0l <junyeolkim00@gmail.com>
2026-01-09 17:38:54 +0900

    feat : custom-plugin setting

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss';
+
+import {
+  flexCenter,
+  flexColCenter,
+} from './src/shared/config/tailwind/plugins';
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: { extend: {} },
+  plugins: [flexCenter, flexColCenter],
+};
+
+export default config;
