bb51df0fbb87027b8957014118ef201afda55352
junye0l <junyeolkim00@gmail.com>
2026-01-09 16:32:01 +0900

    style : 모바일 최대폭 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,12 +12,12 @@ export default function RootLayout({
       <body className="bg-neutral-100">
         {/*
           모바일 전용 컨테이너
-          - max-w-[390px]: 모바일 최대 폭 제한 ( 피그마 디자인 시스템 기준 )
+          - max-w-[430px]: 모바일 최대 폭 제한 ( 아이폰 Pro Max )
           - mx-auto: 데스크톱에서 가운데 정렬
           - min-h-dvh: 모바일 주소창 변화 대응 (vh 대신 dvh)
           - bg-white: 실제 앱 영역
         */}
-        <div className="mx-auto min-h-dvh max-w-[390px] bg-white">
+        <div className="mx-auto min-h-dvh max-w-[430px] bg-white">
           {/* 
             실제 페이지 콘텐츠
             - app/page.tsx 또는 route group의 page.tsx
