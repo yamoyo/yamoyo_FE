b2ded2d46a0612f101f137b3641771790920857f
junye0l <junyeolkim00@gmail.com>
2026-01-20 18:53:28 +0900

    docs : 사용자 프로필 사진 추가

--- src/pages/home/ui/homeList.tsx
+++ src/pages/home/ui/homeList.tsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const HomeList = () => {
-  return <div>팀 목록</div>;
+  return (
+    <div className="flex items-end justify-between px-[24px] pt-[50px]">
+      <span className="text-[16px] font-bold text-[#EEEFF8]">MY 팀룸 목록</span>
+      <Link to="/" className="text-[12px] font-normal text-[#D4C6F8]">
+        전체보기
+      </Link>
+    </div>
+  );
 };
 
 export default HomeList;
---
+++ src/pages/home/ui/homeListItem.tsx
@@ -0,0 +1,5 @@
+const HomeListItem = () => {
+  return <div>팀 리스트 단일 아이템</div>;
+};
+
+export default HomeListItem;
