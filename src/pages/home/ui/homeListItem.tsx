57a1f1794fa1252ed097c91d59220bb12cbed83d
junye0l <junyeolkim00@gmail.com>
2026-01-20 19:31:56 +0900

    style : 홈 뷰 페이지 팀 목록 작업

--- src/pages/home/ui/homeListItem.tsx
+++ src/pages/home/ui/homeListItem.tsx
@@ -1,5 +1,51 @@
-const HomeListItem = () => {
-  return <div>팀 리스트 단일 아이템</div>;
+interface Member {
+  id: number;
+  avatar: string;
+}
+
+interface HomeListItemProps {
+  teamName: string;
+  members: Member[];
+  dday: string;
+}
+
+const HomeListItem = ({ teamName, members, dday }: HomeListItemProps) => {
+  const visibleMembers = members.slice(0, 9);
+
+  return (
+    <div className="flex flex-col gap-[9px] rounded-[12px] border border-[#4C5377] bg-[#3D4366] px-[15px] py-[13px]">
+      <span className="text-[14px] font-bold text-white">{teamName}</span>
+
+      <div className="flex items-center justify-between">
+        {/* 멤버 이미지 */}
+        <div className="flex items-center">
+          <div className="flex items-center -space-x-2">
+            {visibleMembers.map((member) => (
+              <span
+                key={member.id}
+                className="w-6rounded-full h-6 border border-[#3D4366] bg-[#2B2F45] flex-center"
+              >
+                <img
+                  src={member.avatar}
+                  alt="사용자 프로필 이미지"
+                  className="h-[12px] w-[12px]"
+                />
+              </span>
+            ))}
+          </div>
+          {/** TODO(준열): 멤버가 9명을 초과하면 그 이상의 숫자를 나타내는 추가 기능입니다. 확정 X */}
+          {members.length > 9 && (
+            <span className="ml-1 text-[11px] font-medium text-white/70">
+              +{members.length - 9}
+            </span>
+          )}
+        </div>
+
+        {/* D-day */}
+        <p className="text-[11px] font-medium text-white">{dday}</p>
+      </div>
+    </div>
+  );
 };
 
 export default HomeListItem;
