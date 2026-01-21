b0b26567fd61e2bfcaf48f8eea44cea759be6516
jaqwe2301 <jaqwe2301@naver.com>
2026-01-21 19:33:40 +0900

    chore: React 의존성 경고 등을 검사하는 React 관련 eslint 패키지 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 const eslintConfig = defineConfig([
   ...tseslint.configs.recommended,
   {
+    plugins: {
+      'react-hooks': reactHooks,
+    },
     rules: {
+      'react-hooks/rules-of-hooks': 'error', // Hook 규칙 위반 (if 안에서 호출 등)
+      'react-hooks/exhaustive-deps': 'warn', // 의존성 배열 검사 (우리가 원하던 그 경고)
+
       // 사용 안 하는 변수는 경고로 ( _ 로 시작하면 무시 )
       '@typescript-eslint/no-unused-vars': [
         'warn',
