45e4b3b150a9ed45bef0010e96046330ed558071
junye0l <junyeolkim00@gmail.com>
2026-01-22 22:00:32 +0900

    refactor : 고정 상수 별도 파일로 분리

---
+++ src/shared/constants/charImages.ts
@@ -0,0 +1,14 @@
+export const CHARACTER_IMAGES = [
+  '/assets/character/char-1.png',
+  '/assets/character/char-2.png',
+  '/assets/character/char-3.png',
+  '/assets/character/char-4.png',
+  '/assets/character/char-5.png',
+  '/assets/character/char-6.png',
+  '/assets/character/char-7.png',
+  '/assets/character/char-8.png',
+  '/assets/character/char-9.png',
+  '/assets/character/char-10.png',
+  '/assets/character/char-11.png',
+  '/assets/character/char-12.png',
+] as const;
