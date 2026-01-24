dfe3aa10ea00c9d912d546a81c7724d3b06e40b3
jaqwe2301 <jaqwe2301@naver.com>
2026-01-24 21:12:09 +0900

    hotfix: tailwind 타이포그래피, 피그마 설정과 다른 부분 수정

--- src/pages/typography/index.tsx
+++ src/pages/typography/index.tsx
@@ -6,17 +6,25 @@ export function TypographyPage() {
       <p className="font-galmuri-14 text-body-3">여름에</p>
       <br />
       <p className="text-title-1 text-red-500">font Spoqa</p>
-      <p className="text-title-2">title-2 : 22px 24px 700</p>
-      <p className="text-title-3">title-3 : 20px 24px 700</p>
-      <p className="text-body-1">body-1 : 16px 18px 700</p>
-      <p className="text-body-2">body-2 : 16px 18px 600</p>
+      <p className="text-title-1">title-1 : 22px 36px 700</p>
+      <p className="text-title-2">title-2 : 20px 32px 700</p>
+
+      <p className="text-body-1">body-1 : 16px 24px 700</p>
+      <p className="text-body-2">body-2 : 16px 24px 500</p>
       <p className="text-body-3">body-3 : 16px 24px 400</p>
-      <p className="text-body-4">body-4 : 14px 18px 700</p>
-      <p className="text-body-5">body-5 : 14px 18px 600</p>
-      <p className="text-body-6">body-6 : 14px 18px 500</p>
-      <p className="text-body-7">body-7 : 13px 18px 600</p>
-      <p className="text-body-8">body-8 : 13px 18px 700</p>
-      <p className="text-body-9">body-9 : 12px 16px 400</p>
+      <p className="text-body-3.9">body-3.9 : 15px 20px 700</p>
+
+      <p className="text-body-4">body-4 : 14px 21px 700</p>
+      <p className="text-body-4.1">body-4.1 : 14px 21px 500</p>
+      <p className="text-body-5">body-5 : 14px 21px 400</p>
+
+      <p className="text-body-6">body-6 : 13px 20px 400</p>
+      <p className="text-body-7">body-7 : 12px 18px 700</p>
+      <p className="text-body-8">body-8 : 11px 16px 700</p>
+      <p className="text-body-9">body-9 : 11px 16px 400</p>
+
+      <p className="text-caption-1">caption-1 : 12px 16px 400</p>
+      <p className="text-caption-2">caption-2 : 10px 12px 400</p>
     </div>
   );
 }
