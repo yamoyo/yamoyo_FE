3c055d2b945ff345a39224bdc12ca3a79bf2e056
junye0l <junyeolkim00@gmail.com>
2026-01-25 17:40:54 +0900

    style : 캘린더 페이지 헤더 컴포넌트 스타일 일부 수정

--- src/widgets/calendar/ui/CalendarHeader.tsx
+++ src/widgets/calendar/ui/CalendarHeader.tsx
@@ -17,27 +17,33 @@ export default function CalendarHeader({
   return (
     <div className="flex items-center justify-between px-5">
       <div className="flex items-center gap-2">
-        <button onClick={onPrevMonth} className="p-1">
-          <img src={'/assets/icons/arrow-left.svg'} width={9} height={16} />
+        <button onClick={onPrevMonth} className="select-none p-1">
+          <img
+            src={'/assets/icons/arrow-left.svg'}
+            width={9}
+            height={16}
+            draggable="false"
+          />
         </button>
 
         <span className="min-w-[101px] text-center font-galmuri-11 text-title-2 text-white">
           {year}년 {month}월
         </span>
 
-        <button onClick={onNextMonth} className="p-1">
+        <button onClick={onNextMonth} className="select-none p-1">
           <img
             src={'/assets/icons/arrow-left.svg'}
             className="scale-x-[-1]"
             width={9}
             height={16}
+            draggable="false"
           />
         </button>
       </div>
 
       <button
         onClick={onToday}
-        className="rounded-2xl bg-bd-textfiled_line px-4 py-2 text-body-4 text-white"
+        className="rounded-2xl bg-bd-textfiled_line px-[14px] py-[5px] text-body-4 text-tx-default_3"
       >
         오늘
       </button>
