29c199eb22bf005c8c5ed5916e8aa2660689010b
jaqwe2301 <jaqwe2301@naver.com>
2026-01-28 23:43:35 +0900

    feat: 방장 게임 선택 UI 구현

---
+++ src/shared/ui/display/PixelStatusMessage.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/shared/config/tailwind/cn';
+
+export default function PixelStatusMessage({
+  message,
+  className,
+}: {
+  message: string;
+  className?: string;
+}) {
+  return (
+    <div className={cn('select-none space-y-4 flex-col-center', className)}>
+      <img
+        src="/assets/gif/onboarding.gif"
+        alt="animation"
+        className="w-[192px]"
+        draggable={false}
+      />
+      <span className="text-body-1 text-tx-default">{message}</span>
+    </div>
+  );
+}
--- src/widgets/teamroom/leader/model/type.ts
+++ src/widgets/teamroom/leader/model/type.ts
@@ -1,11 +1,7 @@
 export type Phase =
   | 'LEADER_APPLICATION' // 팀장 지원
   | 'LEADER_APPLICATION_WAIT' // 팀장 지원 선택 후 결과 대기
-  | 'AUTO_SELECT_RESULT'
-  | 'SELECT_GAME'
-  | 'GAME_READY'
-  | 'GAME_PLAYING'
-  | 'GAME_RESULT'
+  | 'SELECT_GAME' // 방장이 게임 선택
   | null;
 
 export interface VoteCharacter {
