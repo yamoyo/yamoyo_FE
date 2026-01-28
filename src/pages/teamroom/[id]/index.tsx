56f5947ed7d3eff05cfc44dd31a5a3a45f216b6c
junye0l <junyeolkim00@gmail.com>
2026-01-29 00:29:54 +0900

    feat : 팀장 정하기 게임 시작 섹션 작업

--- src/pages/teamroom/[id]/index.tsx
+++ src/pages/teamroom/[id]/index.tsx
@@ -4,6 +4,7 @@ import TopBar from '@/shared/ui/header/TopBar';
 import { getTeamRoom } from '@/entities/teamroom/api/teamroom-api';
 import type { TeamRoom } from '@/entities/teamroom/model/types';
 import { TEAMROOM_IMAGES } from '@/shared/constants/teamroom-images';
+import BottomButton from '@/shared/ui/button/BottomButton';
 
 export default function TeamRoomMainPage() {
   const navigate = useNavigate();
@@ -150,6 +151,33 @@ export default function TeamRoomMainPage() {
             </li>
           </ul>
         )}
+      </section>
+      <section
+        className="mx-auto flex h-[220px] w-full max-w-[342px] flex-col items-center gap-5 rounded-[12px] border-2"
+        style={{
+          borderColor: 'var(--bg-bg_card, #3D4366)',
+          background:
+            'radial-gradient(395.32% 169.68% at 50% 53.7%, rgba(129, 103, 196, 0.38) 0%, rgba(53, 62, 117, 0.25) 52.99%, rgba(47, 52, 83, 0.77) 100%)',
+        }}
+      >
+        <div className="mt-4 flex w-full flex-col items-start px-5">
+          <span className="text-body-1 text-tx-default">팀장 정하기</span>
+          <span className="text-caption-1 text-tx-default_4">
+            팀원이 모이면 팀장 정하기 게임이 시작됩니다.
+          </span>
+        </div>
+        <img
+          src="/assets/character/char-crown.png"
+          width={78}
+          height={56}
+          alt=""
+        />
+        <BottomButton
+          text="START"
+          onClick={() => {}}
+          // disabled={} 실시간 통신 로직이 있어야 비활성화인지 활성화일지를 판단 가능할 듯 함.
+          className="h-12 w-[302px] gap-[10px] bg-bg-secondary_2 px-[80px] py-[16px] font-galmuri-11 text-[17px] font-bold text-tx-default_black flex-center disabled:bg-tx-default_5 disabled:text-tx-default_4"
+        />
       </section>
     </>
   );
