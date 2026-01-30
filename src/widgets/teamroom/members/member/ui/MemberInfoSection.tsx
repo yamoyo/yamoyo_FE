8bf433d272e370b1a87d87b74411d7ff3da1c157
junye0l <junyeolkim00@gmail.com>
2026-01-30 10:41:27 +0900

    refactor : 멤버별 상세 페이지 공통컴포넌트로 스타일 수정

--- src/widgets/teamroom/members/member/ui/MemberInfoSection.tsx
+++ src/widgets/teamroom/members/member/ui/MemberInfoSection.tsx
@@ -1,4 +1,5 @@
 import { formatDateDot } from '@/entities/calendar/lib/utils/format-date';
+import BasicInfoItem from '@/entities/profile/ui/edit/BasicInfoItem';
 
 interface MemberInfoSectionProps {
   name: string;
@@ -20,30 +21,13 @@ export default function MemberInfoSection({
       className="mb-[172px] flex select-none flex-col items-start gap-[17px] px-6"
       draggable="false"
     >
-      <span className="text-title-2 text-tx-default">기본정보</span>
+      <span className="text-body-1 text-tx-default">기본정보</span>
       <div className="flex flex-col items-start gap-[17px]">
-        <div className="flex gap-[42px]">
-          <span className="w-[42px] text-body-2 text-tx-default_5">이름</span>
-          <span className="text-body-4 text-tx-default">{name}</span>
-        </div>
-        <div className="flex gap-[42px]">
-          <span className="w-full text-body-2 text-tx-default_5">이메일</span>
-          <span className="text-body-4 text-tx-default">{email}</span>
-        </div>
-        <div className="flex gap-[42px]">
-          <span className="w-[42px] text-body-2 text-tx-default_5">전공</span>
-          <span className="text-body-4 text-tx-default">{major}</span>
-        </div>
-        <div className="flex gap-[42px]">
-          <span className="w-[42px] text-body-2 text-tx-default_5">MBTI</span>
-          <span className="text-body-4 text-tx-default">{mbti}</span>
-        </div>
-        <div className="flex gap-[24px]">
-          <span className="w-full text-body-2 text-tx-default_5">가입일자</span>
-          <span className="text-body-4 text-tx-default">
-            {formatDateDot(joinedAt)}
-          </span>
-        </div>
+        <BasicInfoItem label="이름" value={name} />
+        <BasicInfoItem label="이메일" value={email} />
+        <BasicInfoItem label="전공" value={major} />
+        <BasicInfoItem label="MBTI" value={mbti} />
+        <BasicInfoItem label="가입일자" value={formatDateDot(joinedAt)} />
       </div>
     </section>
   );
