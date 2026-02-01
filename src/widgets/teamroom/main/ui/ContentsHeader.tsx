aab0cff3a95f775f7f4702a61c37c6f9a4c90f6d
jaqwe2301 <jaqwe2301@naver.com>
2026-02-01 14:29:26 +0900

    refactor: 팀룸 - 대시보드 콘텐츠 영역 헤더 공통 컴포넌트로 분리

---
+++ src/widgets/teamroom/main/ui/ContentsHeader.tsx
@@ -0,0 +1,44 @@
+interface Props {
+  text: string;
+  id: 'rule' | 'tool' | 'meeting';
+  editMode: boolean;
+  onClickIcon: () => void;
+}
+
+export default function ContentsHeader({
+  text,
+  id,
+  editMode,
+  onClickIcon,
+}: Props) {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        <img
+          className="h-6 w-6"
+          src={`/assets/icons/dashboard/${id}.svg`}
+          alt={`${id} Icon`}
+          draggable={false}
+        />
+        <p className="text-body-2 text-tx-default">{text}</p>
+      </div>
+      <button onClick={onClickIcon} className="p-2.5">
+        {editMode ? (
+          <img
+            className="h-5 w-5"
+            src="/assets/icons/check-active.svg"
+            alt="Check Active Icon"
+            draggable={false}
+          />
+        ) : (
+          <img
+            className="h-5 w-5"
+            src="/assets/icons/dashboard/edit.svg"
+            alt="Edit Icon"
+            draggable={false}
+          />
+        )}
+      </button>
+    </div>
+  );
+}
