53c593f00960641e5d9ad9a8de9eae9519779b11
jaqwe2301 <jaqwe2301@naver.com>
2026-02-17 19:03:21 +0900

    feat: 팀장 미지원자는 게임 참여 X 반영

--- src/shared/ui/display/PixelStatusMessage.tsx
+++ src/shared/ui/display/PixelStatusMessage.tsx
@@ -2,13 +2,21 @@ import { cn } from '@/shared/config/tailwind/cn';
 
 export default function PixelStatusMessage({
   message,
+  fullScreen = false,
   className,
 }: {
   message: string;
+  fullScreen?: boolean;
   className?: string;
 }) {
   return (
-    <div className={cn('select-none space-y-4 flex-col-center', className)}>
+    <div
+      className={cn(
+        'select-none space-y-4 flex-col-center',
+        className,
+        fullScreen && 'flex-1 px-6 pb-[10vh]',
+      )}
+    >
       <img
         src="/assets/gif/onboarding.gif"
         alt="animation"
