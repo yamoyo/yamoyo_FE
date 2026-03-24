2eced44c4142165626b0a6c78d9b5824251e153b
jaqwe2301 <jaqwe2301@naver.com>
2026-03-24 11:51:04 +0900

    style: TopBar 투표 step UI 추가 및 화살표 아이콘 변경

--- src/shared/ui/header/TopBar.tsx
+++ src/shared/ui/header/TopBar.tsx
@@ -15,6 +15,13 @@ interface Props {
   rightContent?: ReactNode;
   /** 게임 폰트를 적용할 때 사용 */
   gameFont?: boolean;
+  /** 숨기기 */
+  hidden?: boolean;
+  /** 단계 */
+  step?: {
+    total: number;
+    current: number;
+  };
 }
 
 /**
@@ -24,7 +31,10 @@ interface Props {
  * @param {boolean} [showBackButton=true] - 뒤로가기 버튼 노출 여부
  * @param {'arrow' | 'cancel'} [backIcon='arrow'] - 뒤로가기 아이콘 타입
  * @param {() => void} [onBack] - 뒤로가기 동작. 미제공 시 navigate(-1)
+ * @param {ReactNode} [rightContent] - 오른쪽에 표시할 커스텀 콘텐츠
  * @param {boolean} [gameFont] - 게임 전용 폰트 적용 여부
+ * @param {boolean} [hidden] - 헤더 숨김 여부
+ * @param {{ total: number; current: number }} [step] - 단계 표시 옵션. total과 current를 포함하는 객체
  *
  * @example
  * // 기본 사용
@@ -45,48 +55,69 @@ export default function TopBar({
   onBack,
   rightContent,
   gameFont,
+  hidden,
+  step,
 }: Props) {
   const navigate = useNavigate();
 
   const handleBack = onBack ?? (() => navigate(-1));
 
+  if (hidden) return null;
+
   return (
-    <header className="relative h-[60px] select-none flex-center">
+    <header className="flex min-h-[44px] select-none items-center justify-between gap-2 px-2 py-2">
       {showBackButton && (
         <button
           onClick={handleBack}
           aria-label={backIcon === 'cancel' ? '닫기' : '뒤로가기'}
           type="button"
-          className="absolute left-[30px] top-1/2 h-10 w-10 -translate-y-1/2 flex-center"
+          className="h-[44px] w-[44px] flex-center"
         >
           <img
             src={
               backIcon === 'cancel'
                 ? '/assets/icons/cancel.svg'
-                : '/assets/icons/arrow-left.svg'
+                : '/assets/icons/arrow-left-pixel.svg'
             }
-            width={backIcon === 'cancel' ? 18 : 10}
-            height={18}
+            width={backIcon === 'cancel' ? 18 : 16}
+            height={backIcon === 'cancel' ? 18 : 16}
             alt={backIcon === 'cancel' ? 'cancel' : 'back'}
             draggable="false"
           />
         </button>
       )}
-      <span
-        className={cn(
-          'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-body-1 text-white',
-          { 'title-g3': gameFont },
-        )}
-        style={gameFont ? { filter: 'drop-shadow(2px 2px 0 #000)' } : undefined}
-      >
-        {title}
-      </span>
+      {title && (
+        <p
+          className={cn('pt-[1px] text-center text-body-1 text-white', {
+            'title-g3': gameFont,
+          })}
+          style={
+            gameFont ? { filter: 'drop-shadow(2px 2px 0 #000)' } : undefined
+          }
+        >
+          {title}
+        </p>
+      )}
 
-      {rightContent && (
-        <div className="absolute right-[22px] top-1/2 -translate-y-1/2">
-          {rightContent}
+      {step && (
+        <div className="flex w-[calc(100%-60px)] flex-1 justify-between gap-0.5">
+          {Array.from({ length: step.total }, (_, index) => (
+            <span
+              key={index}
+              className={cn(
+                'h-1.5 flex-grow rounded-sm',
+                index + 1 === step.current
+                  ? 'bg-bd-textfiled-line_focus'
+                  : 'bg-bg-textfiled',
+              )}
+            />
+          ))}
         </div>
       )}
+
+      <div className="h-[44px] min-w-[44px]">
+        {rightContent && rightContent}
+      </div>
     </header>
   );
 }
