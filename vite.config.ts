239becc543a006e8889a753a9e738c02c3fc5cdd
jaqwe2301 <jaqwe2301@naver.com>
2026-06-09 04:22:44 +0900

    fix: sw.js 관련 캐싱 문제로 인한 로그인 리다이렉트가 안되는 문제

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath } from 'node:url';
 
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 
 // __dirname 대체 (ESM 환경)
@@ -11,57 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['font/*.woff2'],
-      manifest: {
-        name: 'Yamoyo', // 설치 배너에 표시되는 이름
-        short_name: 'Yamoyo', // 앱 아이콘 아래에 표시되는 이름
-        description: '모이자마자 완성되는 팀 세팅 플랫폼', // 프로젝트 설명
-        theme_color: '#ffffff',
-        background_color: '#f5f5f5',
-        display: 'standalone',
-        orientation: 'portrait',
-        start_url: '/',
-        icons: [
-          {
-            src: '/web-app-manifest-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: '/web-app-manifest-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'], // 빌드 시 캐싱할 파일 패턴
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10, // 캐싱할 항목에 대한 최대 갯수
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1년간 캐싱 유지
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
